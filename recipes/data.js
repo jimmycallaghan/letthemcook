c687d578d5a4a4894459248c22e4deae87a3708b
James Callaghan <jcallaghan@trv7830.fritz.box>
2024-06-01 19:31:08 +0200

    Moved the data to single js file

---
+++ recipes/data.js
@@ -0,0 +1,179 @@
+var recipes = [
+    {
+        name: "BBQ-Chicken-Nachos",
+        ingredients: [
+            {
+            "name": "Oel",
+            "amount": "1",
+            "unit": "TL"
+        }, {
+            "name": "Huehnerbrustfilet(s)",
+            "amount": "2",
+            "unit": "große"
+        }, {
+            "name": "Salz, grob",
+            "amount": "1/2",
+            "unit": "TL"
+        }, {
+            "name": "Pfeffer",
+            "amount": "1/2",
+            "unit": "TL"
+        }, {
+            "name": "Knoblauchpulver",
+            "amount": "1",
+            "unit": "TL"
+        }, {
+            "name": "Chilipulver",
+            "amount": "1",
+            "unit": "TL"
+        }, {
+            "name": "Barbecuesauce",
+            "amount": "200",
+            "unit": "ml"
+        }, {
+            "name": "Tortillachips",
+            "amount": "1",
+            "unit": "Pck."
+        }, {
+            "name": "Käse, geriebener, z.B. Gouda, Cheddar",
+            "amount": "200",
+            "unit": "g"
+        }, {
+            "name": "Zwiebel(n), rote, gewürfelt",
+            "amount": "1/2",
+            "unit": "große"
+        }, {
+            "name": "Tomate(n), gewürfelt",
+            "amount": "1",
+            "unit": "große"
+        }, {
+            "name": "Schicken, gewürfelt",
+            "amount": "8",
+            "unit": "Scheibe/n"
+        }, {
+            "name": "Petersilie, gehackt",
+            "amount": "2",
+            "unit": "TL"
+        }, {
+            "name": "saure Sahne",
+            "amount": "120",
+            "unit": "ml"
+        }
+        ],
+        nutrition: [
+            {
+            "name": "kcal",
+            "amount": "306"
+        }, {
+            "name": "Eiweiß",
+            "amount": "17g"
+        }, {
+            "name": "Fett",
+            "amount": "34g"
+        }, {
+            "name": "Kohlenhydrate",
+            "amount": "58g"
+        }
+        ],
+        method: [
+            "Öl in einer Pfanne stark erhitzen. Zuerst Hühnerbrust-Würfel anbraten, dann Salz, " +
+            "Pfeffer, Knoblauchpulver und Chilipulver hinzufügen. Gut umrühren.Barbecuesauce hinzufügen " +
+            "und 5 Minuten köcheln lassen.",
+            "In einer tiefen Bratpfanne 2 Hände voll Tortilla-Chips verteilen und mit der Hälfte " +
+            "des marinierten Hühnchens belegen. Käse, Zwiebel, Tomate, Schinken und " +
+            "Petersilie darüber verteilen. Anschließend den Vorgang nochmal wiederholen.",
+            "Bei 175 °C Ober-/Unterhitze für 15 Minuten backen.",
+            "Zum Servieren saure Sahne darauf geben."
+            ]
+    },
+    {
+        name: "Quesadilla",
+        ingredients: [
+            {
+                "name" : "Hähnchenbrustfilet(s)",
+                "amount" : "400",
+                "unit" : "g"
+            },{
+                "name" : "Kreuzkümmelpulver",
+                "amount" : "1",
+                "unit" : "TL"
+            },{
+                "name" : "Salz",
+                "amount" : "2",
+                "unit" : "TL"
+            },{
+                "name" : "Pfeffer",
+                "amount" : "1/2",
+                "unit" : "TL"
+            },{
+                "name" : "Paprikapulver",
+                "amount" : "1",
+                "unit" : "EL"
+            },{
+                "name" : "Öl",
+                "amount" : "3",
+                "unit" : "EL"
+            },{
+                "name" : "Paprikaschote(n), rote und grüne",
+                "amount" : "2",
+                "unit" : "größe"
+            },{
+                "name" : "Zwiebel(n), rot",
+                "amount": "1",
+                "unit" : "große"
+            },{
+                "name" : "Knoblauchzehe(n)",
+                "amount" : "1",
+                "unit" : "große"
+            },{
+                "name" : "Limette(n), saft",
+                "amount" : "1",
+                "unit" : "große"
+            },{
+                "name" : "Käse, geriebener",
+                "amount" : "200",
+                "unit" : "g"
+            },{
+                "name" : "Weizentortilla(s)",
+                "amount" : "6",
+                "unit" : "große"
+            },{
+                "name" : "Butter, geschmolzen",
+                "amount" : "2",
+                "unit" : "EL"
+            }
+        ],
+        nutrition: [
+            {
+                "name": "kcal",
+                "amount" : "340"
+            },{
+                "name" : "Eiweiß",
+                "amount" : "14g"
+            },{
+                "name" : "Fett",
+                "amount" : "14g"
+            },{
+                "name" : "Kohlenhydrate",
+                "amount" : "27g"
+            }
+        ],
+        method: [
+            "Das Hähnchenfleisch klein würfeln, mit Kreuzkümmel, Paprikapulver, " +
+            "Salz und Pfeffer würzen und vermischen.",
+            "Paprikaschoten und Zwiebel ebenfalls fein würfeln und die " +
+            "Knoblauchzehe fein reiben.",
+            "In einer beschichteten Pfanne 2 EL Öl erhitzen und das " +
+            "Hähnchenfleisch unter Rühren braten und anschließend in eine " +
+            "Schüssel geben. In derselben Pfanne 1 EL Öl erhitzen und Paprika, " +
+            "Zwiebel und Knoblauchzehe andünsten. Gemüse herausnehmen und " +
+            "zum Hähnchenfleisch geben. Limettensaft dazugeben und verrühren.",
+            "Eine Hälfte der Tortillas mit geschmolzener Butter bestreichen. Die " +
+            "Fleischpfanne kurz auswischen und erhitzen. Eine Tortilla in die " +
+            "Pfanne geben (mit Butter bestrichene Seite sollte unten sein). Fleisch- " +
+            "Gemüse-Mischung auf der Hälfte verteilen und Käse darüberstreuen." +
+            "Andere Tortillahälfte darüber klappen. Von beiden Seiten knusprig braten.",
+            "Mit Guacamole servieren."
+        ]
+    }
+]
--- recipes/recipes.js
+++ recipes/recipes.js
@@ -28,23 +28,72 @@ function buildrecipe(recipe) {
         
 }}
 
-function buildNährwerte(Nährwerte) {
+function buildNaehrwerte(Naehrwerte) {
     // Loop around to the next element in the array list and repeat
 
-    for (let i = 0; i <Nährwerte.length; i++) {
+    for (let i = 0; i <Naehrwerte.length; i++) {
 
         var newNutritionalValuesToAdd = '';
         
         newNutritionalValuesToAdd = '<tr>';
-        newNutritionalValuesToAdd += '<td>' + Nährwerte[i].amount + '</td>';
-        newNutritionalValuesToAdd += '<td>' + Nährwerte[i].name + '</td>';
+        newNutritionalValuesToAdd += '<td>' + Naehrwerte[i].amount + '</td>';
+        newNutritionalValuesToAdd += '<td>' + Naehrwerte[i].name + '</td>';
         newNutritionalValuesToAdd += '</tr>';
 
         $('#NutritionalValuesTable').append(newNutritionalValuesToAdd);
 }}
 
+function buildIngredients(ingredients) {
+    // Loop around to the next element in the array list and repeat
+
+    for (let i = 0; i <ingredients.length; i++) {
+
+        var newNutritionalValuesToAdd = '';
+
+        newNutritionalValuesToAdd = '<tr>';
+        newNutritionalValuesToAdd += '<td>' + ingredients[i].amount + ' ' + ingredients[i].unit + '</td>';
+        newNutritionalValuesToAdd += '<td>' + ingredients[i].name + '</td>';
+        newNutritionalValuesToAdd += '</tr>';
+
+        $('#ingredientTable').append(newNutritionalValuesToAdd);
+    }}
+
+function buildMethod(method) {
+    // Loop around to the next element in the array list and repeat
+
+    for (let i = 0; i <method.length; i++) {
+
+        // We need to decide, for this number i, whether it should use the tableOdd
+        // or tableEven class. We default to tableOdd (which is defined in the css
+        // to have a background color of white).
+        let trClass = 'tableOdd';
+
+        // If we perform the operation i%2, on even numbers we will get a result of
+        // zero. On odd numbers we will get a result of 1. If we get a zero, change
+        // the class to tableEven.
+        if (i%2 === 0) {
+            trClass = 'tableEven';
+        }
+
+        var newStepToAdd = '';
+
+        newStepToAdd = '<tr class="' + trClass + '">';
+        newStepToAdd += '<td class="methodStepCol1">Step ' + (i+1) + ':</td>';
+        newStepToAdd += '<td class="methodStepCol2">' + method[i] + '</td>';
+        newStepToAdd += '</tr>';
+
+        $('#methodTable').append(newStepToAdd);
+    }}
+
 function buildTitle(title) {
 
     $('#titel').append(title);
 
+}
+
+function buildPage(recipeId) {
+    buildTitle(recipes[recipeId].name);
+    buildIngredients(recipes[recipeId].ingredients);
+    buildNaehrwerte(recipes[recipeId].nutrition);
+    buildMethod(recipes[recipeId].method);
 }
