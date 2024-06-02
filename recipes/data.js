88dc7ea2979fb48b433aaf24f5204db28484bec0
jimmycallaghan <jimbocallaghan@gmail.com>
2024-06-02 18:48:16 +0200

    i added 2 new recipes (burritos and Tacos) to the data json file

--- recipes/data.js
+++ recipes/data.js
@@ -1,5 +1,294 @@
 var recipes = [
     {
+        name: "Burrito",
+        ingredients: [
+            {
+                "name" : "Tomaten",
+                "amount" : "200",
+                "unit" : "g"
+            },{
+                "name" : "Maiskörner",
+                "amount" : "125",
+                "unit" : "g"
+            },{
+                "name" : "Paprikaschote(n), rote",
+                "amount" : "1",
+                "unit" : "ganze"
+            },{
+                "name" : "Paprikaschote(n), grüne",
+                "amount" : "1",
+                "unit" : "ganze"
+            },{
+                "name" : "Zwiebeln",
+                "amount" : "2",
+                "unit" : "große"
+            },{
+                "name" : "Olivenöl",
+                "amount" : "4",
+                "unit" : "EL"
+            },{
+                "name" : "Hackfleisch",
+                "amount" : "400",
+                "unit" : "g"
+            },{
+                "name" : "salz und Pfeffer",
+                "amount": "1",
+                "unit" : "Prise(n)"
+            },{
+                "name" : "Cayennepfeffer",
+                   "amount" : "1",
+                "unit" : "prise(n)"
+            },{
+                "name" : "Kreuzkümmelpulver",
+                "amount" : "1/2",
+                "unit" : "TL"
+            },{
+                "name" : "Tomatenmarkt",
+                "amount" : "1",
+                "unit" : "EL"
+            },{
+                "name" : "Zucker",
+                "amount" : "1",
+                "unit" : "Prise(n)"
+            },{
+                "name" : "Weizentrtoilla(s)",
+                "amount" : "6",
+                "unit" : "große"
+            },{
+                "name" : "Crème fraîche",
+                "amount" : "6",
+                "unit" : "EL"
+            },{
+                "name" : "Käse, gerieben",
+                "amount" : "200",
+                "unit" : "g"
+            }
+                ],
+        nutrition: [
+            {
+                    "name": "kcal",
+                    "amount" : "900"
+            },{
+                    "name" : "Fett",
+                    "amount" : "38,8g"
+            },{
+                    "name" : "Kohlenhydrate",
+                    "amount" : "67,3g"
+            },{
+                    "name" : "Eiweiß",
+                    "amount" : "6g"
+            }
+        ],
+        method: [
+            "Die Tomaten fein hacken und wieder in den Saft zurückgeben. Den Mais auf einem " +
+            "Sieb abtropfen lassen. Die Paprikaschoten putzen, entkernen, waschen, trocknen " +
+            "und in kleine Stücke schneiden. Die Zwiebeln schälen und würfeln.",
+            
+            "Das Olivenöl in einer Pfanne erhitzen und das Hackfleisch darin scharf anbraten. " +
+            "Zwiebeln und Paprika dazugeben und alles etwa 5 Minuten garen, bis das Fleisch " +
+            "durchgebraten ist. Mit Salz, Pfeffer, Cayennepfeffer und Kreuzkümmel kräftig würzen " +
+            "Das Tomatenmark dazugeben, kurz mitrösten, dann die gehackten Tomaten mit dem Saft " +
+            "und den Mais unterrühren. Die Masse einige Minuten einkochen lassen, bis fast die " +
+            "gesamte Flüssigkeit verdampft ist.",
+            
+            "Mit den Gewürzen und etwas Zucker abschmecken. Die Tortillas mit der Crème fraîche " +
+            "bestreichen und die Hackfleischmasse gleichmäßig darauf verteilen. Mit etwas geriebenem " +
+            "Käse bestreuen. Die Tortillas zu Burritos aufrollen und in eine Auflaufform legen. " +
+            "Mit dem restlichen geriebenen Käse bestreuen.",
+            
+            "Bei 200 °C Ober-/Unterhitze im vorgeheizten Backofen ca. 4 - 5 Minuten überbacken, " +
+            "bis der Käse zerlaufen ist. Sofort servieren. "
+        ]
+    },{
+        name: "Tacos",
+        ingredients: [
+                {    
+                "name" : "Öl",
+                "amount" : "1",
+                "unit" : "EL"
+            },{
+                "name" : "Zwiebel(n)",
+                "amount" : "1",
+                "unit" : "große"
+            },{
+                "name" : "Knoblauchzehe(n)",
+                "amount" : "2",
+                "unit" : "große"
+            },{
+                "name" : "Tinderhackfleisch, mageres",
+                "amount" : "500",
+                "unit" : "g"
+            },{
+                "name" : "Tomatenmark",
+                "amount" : "2",
+                "unit" : "EL"
+            },{
+                "name" : "Chilipulver",
+                "amount" : "1/2",
+                "unit" : "EL"
+            },{
+                "name" : "Paprikapulver",
+                "amount" : "1",
+                "unit" : "TL"
+            },{
+                "name" : "Kreuzkümmelpulver",
+                "amount": "1",
+                "unit" : "TL"
+            },{
+                "name" : "Korianderpulver",
+                "amount" : "1",
+                "unit" : "TL"
+            },{
+                "name" : "Oregano",
+                "amount" : "1/2",
+                "unit" : "TL"
+            },{
+                "name" : "Zucker",
+                "amount" : "1",
+                "unit" : "TL"
+            },{
+                "name" : "Salz",
+                "amount" : "1",
+                "unit" : "Prise"
+            },{
+                "name" : "Tacos (schalen)",
+                "amount" : "12",
+                "unit" : "kleine"
+            },{
+                "name" : "Eisbergsalat (große Blätter), zerkleinert",
+                "amount" : "4",
+                "unit" : "Blätter"
+            },{
+                "name" : "Cheddarkäse, geriebener",
+                "amount" : "190",
+                "unit" : "g"
+            },{
+                "name" : "Salsa (Tomatensalsa)",
+                "amount" : "100",
+                "unit" : "g"
+            }
+        ],
+        nutrition: [
+            {
+                "name": "kcal",
+                "amount" : "114"
+            },{
+                "name" : "Eiweiß",
+                "amount" : "9g"
+            },{
+                "name" : "Fett",
+                "amount" : "4g"
+            },{
+                "name" : "Kohlenhydrate",
+                "amount" : "10g"
+    }
+        ],
+        method: [
+            "Reis, Kokosmilch und Brühe in einen Topf geben und aufkochen. Hitze reduzieren und " +
+            "bei geschlossenem Deckel 10 Min. köcheln lassen. Limette und Koriander waschen " +
+            "und trocknen. Von der Limette die Schale fein abreiben und den Saft auspressen. " +
+            "Koriander hacken. Cashewkerne ebenfalls grob hacken. Alles unter den Reis rühren. " +
+            "Tomaten grob würfeln.",
+            
+            "Etwas Öl in einer Pfanne erhitzen. Das Hackfleisch darin bei starker Hitze rundherum " +
+            "krümelig braten. Mit Salz, Pfeffer und Cayennepfeffer würzen.",
+            
+            "Tacoschalen mit Reis, Tomaten und dem Hackfleisch füllen, einen Klecks Crème" +
+            "fraîche dazugeben."
+        ]
+    },{    
+        name: "Quesadilla",
+        ingredients: [
+            {
+                "name" : "Hähnchenbrustfilet(s)",
+                "amount" : "400",
+                "unit" : "g"
+            },{
+                "name" : "Kreuzkümmelpulver",
+                "amount" : "1",
+                "unit" : "TL"
+            },{
+                "name" : "Salz",
+                "amount" : "2",
+                "unit" : "TL"
+            },{
+                "name" : "Pfeffer",
+                "amount" : "1/2",
+                "unit" : "TL"
+            },{
+                "name" : "Paprikapulver",
+                "amount" : "1",
+                "unit" : "EL"
+            },{
+                "name" : "Öl",
+                "amount" : "3",
+                "unit" : "EL"
+            },{
+                "name" : "Paprikaschote(n), rote und grüne",
+                "amount" : "2",
+                "unit" : "größe"
+            },{
+                "name" : "Zwiebel(n), rot",
+                "amount": "1",
+                "unit" : "große"
+            },{
+                "name" : "Knoblauchzehe(n)",
+                "amount" : "1",
+                "unit" : "große"
+            },{
+                "name" : "Limette(n), saft",
+                "amount" : "1",
+                "unit" : "große"
+            },{
+                "name" : "Käse, geriebener",
+                "amount" : "200",
+                "unit" : "g"
+            },{
+                "name" : "Weizentortilla(s)",
+                "amount" : "6",
+                "unit" : "große"
+            },{
+                "name" : "Butter, geschmolzen",
+                "amount" : "2",
+                "unit" : "EL"
+            }
+        ],
+        nutrition: [
+            {
+                "name": "kcal",
+                "amount" : "340"
+            },{
+                "name" : "Eiweiß",
+                "amount" : "14g"
+            },{
+                "name" : "Fett",
+                "amount" : "14g"
+            },{
+                "name" : "Kohlenhydrate",
+                "amount" : "27g"
+            }
+        ],
+        method: [
+            "Das Hähnchenfleisch klein würfeln, mit Kreuzkümmel, Paprikapulver, " +
+            "Salz und Pfeffer würzen und vermischen.",
+            "Paprikaschoten und Zwiebel ebenfalls fein würfeln und die " +
+            "Knoblauchzehe fein reiben.",
+
+            "In einer beschichteten Pfanne 2 EL Öl erhitzen und das " +
+            "Hähnchenfleisch unter Rühren braten und anschließend in eine " +
+            "Schüssel geben. In derselben Pfanne 1 EL Öl erhitzen und Paprika, " +
+            "Zwiebel und Knoblauchzehe andünsten. Gemüse herausnehmen und " +
+            "zum Hähnchenfleisch geben. Limettensaft dazugeben und verrühren.",
+
+            "Eine Hälfte der Tortillas mit geschmolzener Butter bestreichen. Die " +
+            "Fleischpfanne kurz auswischen und erhitzen. Eine Tortilla in die " +
+            "Pfanne geben (mit Butter bestrichene Seite sollte unten sein). Fleisch- " +
+            "Gemüse-Mischung auf der Hälfte verteilen und Käse darüberstreuen." +
+            "Andere Tortillahälfte darüber klappen. Von beiden Seiten knusprig braten.",
+
+            "Mit Guacamole servieren."
+        ]
+    },{
         name: "BBQ-Chicken-Nachos",
         ingredients: [
             {
@@ -79,101 +368,14 @@ var recipes = [
             "Öl in einer Pfanne stark erhitzen. Zuerst Hühnerbrust-Würfel anbraten, dann Salz, " +
             "Pfeffer, Knoblauchpulver und Chilipulver hinzufügen. Gut umrühren.Barbecuesauce hinzufügen " +
             "und 5 Minuten köcheln lassen.",
+
             "In einer tiefen Bratpfanne 2 Hände voll Tortilla-Chips verteilen und mit der Hälfte " +
             "des marinierten Hühnchens belegen. Käse, Zwiebel, Tomate, Schinken und " +
             "Petersilie darüber verteilen. Anschließend den Vorgang nochmal wiederholen.",
+
             "Bei 175 °C Ober-/Unterhitze für 15 Minuten backen.",
+
             "Zum Servieren saure Sahne darauf geben."
             ]
     },
-    {
-        name: "Quesadilla",
-        ingredients: [
-            {
-                "name" : "Hähnchenbrustfilet(s)",
-                "amount" : "400",
-                "unit" : "g"
-            },{
-                "name" : "Kreuzkümmelpulver",
-                "amount" : "1",
-                "unit" : "TL"
-            },{
-                "name" : "Salz",
-                "amount" : "2",
-                "unit" : "TL"
-            },{
-                "name" : "Pfeffer",
-                "amount" : "1/2",
-                "unit" : "TL"
-            },{
-                "name" : "Paprikapulver",
-                "amount" : "1",
-                "unit" : "EL"
-            },{
-                "name" : "Öl",
-                "amount" : "3",
-                "unit" : "EL"
-            },{
-                "name" : "Paprikaschote(n), rote und grüne",
-                "amount" : "2",
-                "unit" : "größe"
-            },{
-                "name" : "Zwiebel(n), rot",
-                "amount": "1",
-                "unit" : "große"
-            },{
-                "name" : "Knoblauchzehe(n)",
-                "amount" : "1",
-                "unit" : "große"
-            },{
-                "name" : "Limette(n), saft",
-                "amount" : "1",
-                "unit" : "große"
-            },{
-                "name" : "Käse, geriebener",
-                "amount" : "200",
-                "unit" : "g"
-            },{
-                "name" : "Weizentortilla(s)",
-                "amount" : "6",
-                "unit" : "große"
-            },{
-                "name" : "Butter, geschmolzen",
-                "amount" : "2",
-                "unit" : "EL"
-            }
-        ],
-        nutrition: [
-            {
-                "name": "kcal",
-                "amount" : "340"
-            },{
-                "name" : "Eiweiß",
-                "amount" : "14g"
-            },{
-                "name" : "Fett",
-                "amount" : "14g"
-            },{
-                "name" : "Kohlenhydrate",
-                "amount" : "27g"
-            }
-        ],
-        method: [
-            "Das Hähnchenfleisch klein würfeln, mit Kreuzkümmel, Paprikapulver, " +
-            "Salz und Pfeffer würzen und vermischen.",
-            "Paprikaschoten und Zwiebel ebenfalls fein würfeln und die " +
-            "Knoblauchzehe fein reiben.",
-            "In einer beschichteten Pfanne 2 EL Öl erhitzen und das " +
-            "Hähnchenfleisch unter Rühren braten und anschließend in eine " +
-            "Schüssel geben. In derselben Pfanne 1 EL Öl erhitzen und Paprika, " +
-            "Zwiebel und Knoblauchzehe andünsten. Gemüse herausnehmen und " +
-            "zum Hähnchenfleisch geben. Limettensaft dazugeben und verrühren.",
-            "Eine Hälfte der Tortillas mit geschmolzener Butter bestreichen. Die " +
-            "Fleischpfanne kurz auswischen und erhitzen. Eine Tortilla in die " +
-            "Pfanne geben (mit Butter bestrichene Seite sollte unten sein). Fleisch- " +
-            "Gemüse-Mischung auf der Hälfte verteilen und Käse darüberstreuen." +
-            "Andere Tortillahälfte darüber klappen. Von beiden Seiten knusprig braten.",
-            "Mit Guacamole servieren."
-        ]
-    }
 ]
